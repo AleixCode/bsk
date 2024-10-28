dd08c0abe846b20e8289818ed5fe58409e7a2b90
Aleix L <aleixcode@gmail.com>
2024-10-28 15:06:34 +0000

    user story 2

--- bowling.py
+++ bowling.py
@@ -8,13 +8,20 @@ class BowlingGame:
         self.frames = []
     
     def add_frame(self, frame: Frame) -> None:
+        if 10 <= len(self.frames):
+            raise BowlingError
         self.frames.append(frame)
 
     def get_frame_at(self, i: int) -> Frame:
+        if i >= len(self.frames):
+            raise BowlingError
         return self.frames[i]
 
     def calculate_score(self) -> int:
-        pass
+        total = 0
+        for frame in self.frames:
+            total += frame.score()
+        return total
 
     def set_first_bonus_throw(self, bonus_throw: int) -> None:
         pass
--- test/test_bowling.py
+++ test/test_bowling.py
@@ -12,3 +12,66 @@ class TestBowlingGame(unittest.TestCase):
         game = BowlingGame()
         game.add_frame(f)
         self.assertEqual(f, game.get_frame_at(0))
+
+    def test_game_empty(self):
+        game = BowlingGame()
+        self.assertRaises(BowlingError, game.get_frame_at, 0)
+
+    def test_game_0_frames(self):
+        f = Frame(1,5)
+        game = BowlingGame()
+        self.assertEqual(0, game.calculate_score())
+
+    def test_game_points_sum(self):
+        game = BowlingGame()
+        f0 = Frame(1, 5)
+        f1 = Frame(3,6)
+        f2 = Frame(7,2)
+        f3 = Frame(3,6)
+        f4 = Frame(4,4)
+        f5 = Frame(5,3)
+        f6 = Frame(3,3)
+        f7 = Frame(4,5)
+        f8 = Frame(8,1)
+        f9 = Frame(2,6)
+
+        game.add_frame(f0)
+        game.add_frame(f1)
+        game.add_frame(f2)
+        game.add_frame(f3)
+        game.add_frame(f4)
+        game.add_frame(f5)
+        game.add_frame(f6)
+        game.add_frame(f7)
+        game.add_frame(f8)
+        game.add_frame(f9)
+
+        self.assertEqual(81, game.calculate_score())
+
+    def test_game_more_than_10_frames(self):
+        game = BowlingGame()
+        f0 = Frame(1, 5)
+        f1 = Frame(3,6)
+        f2 = Frame(7,2)
+        f3 = Frame(3,6)
+        f4 = Frame(4,4)
+        f5 = Frame(5,3)
+        f6 = Frame(3,3)
+        f7 = Frame(4,5)
+        f8 = Frame(8,1)
+        f9 = Frame(2,6)
+        f10 = Frame(2, 6)
+
+        game.add_frame(f0)
+        game.add_frame(f1)
+        game.add_frame(f2)
+        game.add_frame(f3)
+        game.add_frame(f4)
+        game.add_frame(f5)
+        game.add_frame(f6)
+        game.add_frame(f7)
+        game.add_frame(f8)
+        game.add_frame(f9)
+
+        self.assertRaises(BowlingError, game.add_frame, f10)
+
